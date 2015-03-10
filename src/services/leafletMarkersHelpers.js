2e03c8a85a2c0a1fce3881b388ac8523a52b868a
Rufus Honour <roh2@kent.ac.uk>
2015-03-10 22:15:45 +0000

    .

--- src/services/leafletMarkersHelpers.js
+++ src/services/leafletMarkersHelpers.js
@@ -165,6 +165,7 @@ angular.module("leaflet-directive").factory('leafletMarkersHelpers', function ($
             }
 
             if(markerData.nodes == undefined || markerData.nodes.length == 0) {
+                markerData.nodes = [];
                 markerData.nodes.push([markerData.lat, markerData.lng]);
             }
             
